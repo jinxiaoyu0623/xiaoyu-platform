e039bd1b6a4920870e0a2708885b4c0b6fa56dca
jinxiaoyu <whxiaoyu123@gmail.com>
2019-07-04 14:47:59 +0800

    add spring-cloud bus

--- xiaoyu-service/src/main/java/org/xiaoyu/service/XiaoyuServiceApplication.java
+++ xiaoyu-service/src/main/java/org/xiaoyu/service/XiaoyuServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableDiscoveryClient
